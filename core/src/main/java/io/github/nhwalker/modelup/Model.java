a0230a88cfec56ded8f2cc724f1353814b959ec1
N.H. Walker <42497130+nhwalker@users.noreply.github.com>
2021-06-10 20:46:40 -0400

    Setup Projects

---
+++ core/src/main/java/io/github/nhwalker/modelup/Model.java
@@ -0,0 +1,5 @@
+package io.github.nhwalker.modelup;
+
+public interface Model {
+
+}
---
+++ examples-jre16/src/main/java/io/github/nhwalker/modelup/examples/jre16/TestMain.java
@@ -0,0 +1,12 @@
+package io.github.nhwalker.modelup.examples.jre16;
+
+import io.github.nhwalker.modelup.Model;
+
+public class TestMain {
+
+  public static void main(String[] args) {
+    System.out.println(Model.class);
+
+  }
+
+}
